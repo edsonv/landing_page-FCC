b78f05035b6579bdf6c31e5bdaa639f7ea31128b
Edson J Vargas <edson.jhair@yahoo.com>
2018-08-26 12:56:05 -0400

    Se agrego la opcion de peso de fuente de 700

--- postcss.config.js
+++ postcss.config.js
@@ -1,25 +1,32 @@
-
 module.exports= {
-  plugins: [
-  require('postcss-import')( {
+  plugins: [ require('postcss-import')( {
     plugins: [ require('stylelint')]
-  }),
+  }
+  ),
   require('postcss-font-magician')( {
     variants: {
       'Lato': {
         '300': [],
         '400': [],
+        '700': []
       }
     }
-  }),
+  }
+  ),
   require('postcss-preset-env')( {
-    stage: 2,
+    stage: 0,
     features: {
       customProperties: false,
-      calc: false
-    },
+      calc: false,
+      autoprefixer: {
+        grid: true,
+        flexbox:true
+      }
+    }
+    ,
     browsers: 'last 2 versions'
-  }),
+  }
+  ),
   require('css-mqpacker'),
   require('cssnano'),
   ]
